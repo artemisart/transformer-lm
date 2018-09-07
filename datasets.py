a0e45cd8c03a15f28eed69d4095c941cf3837fc7
Mathis Chenuet <mathis.chenuet@unchartech.com>
2018-09-07 19:31:50 +0200

    imdb loading

--- datasets.py
+++ datasets.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import numpy as np
+from pathlib import Path
 
 from tqdm import tqdm
 
@@ -9,9 +10,29 @@ from sklearn.model_selection import train_test_split
 
 seed = 3535999445
 
-def imdb():
-    from pathlib import Path
-    raise NotImplementedError
+def imdb(path=Path("data/aclImdb/")):
+    import pickle
+
+    try:
+        return pickle.load((path / "train-test.p").open("rb"))
+    except FileNotFoundError:
+        pass
+
+    CLASSES = ["neg", "pos", "unsup"]
+    def get_texts(path):
+        texts,labels = [],[]
+        for idx,label in tqdm(enumerate(CLASSES)):
+            for fname in tqdm((path/label).glob('*.txt'), leave=False):
+                texts.append(fname.read_text())
+                labels.append(idx)
+        return texts, np.asarray(labels)
+    
+    trXY = get_texts(path / "train")
+    teXY = get_texts(path / "test")
+    data = (trXY, teXY)
+    pickle.dump(data, (path / "train-test.p").open("wb"))
+    return data
+
 
 def _rocstories(path):
     """Returns 4 lists :
